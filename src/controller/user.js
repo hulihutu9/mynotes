b72fb483d1103b536c718749acf9fbc6112ce947
hutu <huozhiniao@sina.com>
2018-05-18 18:19:59 +0800

    修改格式

--- src/controller/user.js
+++ src/controller/user.js
@@ -8,10 +8,10 @@ module.exports = class extends Base {
   async loginAction() {
     // 如果是POST过来登录信息
     if (this.isPost) {
-      let name = this.post('name');
-      let password = this.post('password');
+      const name = this.post('name');
+      const password = this.post('password');
 
-      let data = await this.model('user').where({name:name, password:password}).find();
+      const data = await this.model('user').where({ name: name, password: password }).find();
       // 用户名、密码不正确
       if (think.isEmpty(data)) {
         this.assign('error', '用户名或密码错误');
